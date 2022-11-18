b3a41b48ebb8acd8c62737d145ea48478bd82dbe
Gabriel <gopxfs@hotmail.com>
2022-11-18 20:11:49 -0300

    Update painting block method Add togglePaint() to toggle both activate/desactivate paint Add paintSquare() to paint the current square Add addColor() to paint all the squares

--- components/paintSquares.js
+++ components/paintSquares.js
@@ -1,11 +1,37 @@
-const addColor = (square) => {
+let toggled = false;
+
+const togglePaint = () => {
+  const squares = document.querySelectorAll(".grid-container__square");
+
+  if (toggled) {
+    squares.forEach((square) => {
+      square.removeEventListener("mouseover", addColor, true)
+      square.square = square
+    })
+  } else {
+    squares.forEach((square) => {
+      square.addEventListener("mouseover", addColor, true)
+      square.square = square
+    })
+  }
+
+  toggled = !toggled;
+}
+
+const paintSquare = (square) => {
   square.style.backgroundColor = 'red';
+}
+
+const addColor = (event) => {
+  event.currentTarget.square.style.backgroundColor = 'red';
 }
 
 export const paintSquares = () => {
   const squares = document.querySelectorAll(".grid-container__square");
   squares.forEach((square) => {
-    square.addEventListener("mouseover", () => addColor(square))
+    square.addEventListener("mousedown", () => paintSquare(square), true)
+    square.addEventListener("mousedown", togglePaint, true)
+    square.addEventListener("mouseup", togglePaint, true)
   })
 }
 
